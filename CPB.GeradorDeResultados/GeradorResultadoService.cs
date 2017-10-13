1e940cfcb425a5a3ca1d84bf5a39dfc147adabaf
Diego Neves <diegoneves1989@gmail.com>
2017-10-12 21:44:53 -0300

    correção de problema de arquivo usado por outro processo.

--- CPB.GeradorDeResultados/GeradorResultadoService.cs
+++ CPB.GeradorDeResultados/GeradorResultadoService.cs
@@ -1,6 +1,5 @@
 ﻿using CPB.GeradorDeResultados.DTO;
 using System;
-using System.Collections.Generic;
 using System.Configuration;
 using System.Diagnostics;
 using System.IO;
@@ -8,14 +7,11 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Timers;
 using System.Windows.Forms;
-using System.Linq;
-using System.Threading;
 
 namespace CPB.GeradorDeResultados
 {
     public class GeradorResultadoService
     {
-        FileSystemWatcher _watcher = null;
         string _pasta = string.Empty;
         string _pastaResultadosExibidos = string.Empty;
         string _pastaPaginas = string.Empty;
@@ -23,7 +19,8 @@ namespace CPB.GeradorDeResultados
         Process _internetExplorer = null;
         DateTime _horaInicio;
         string[] _linhasArquivo;
-        private readonly System.Timers.Timer _timer;
+        private readonly System.Timers.Timer _timerResultados;
+        private readonly System.Timers.Timer _timerPrincipal;
 
         [DllImport("user32.dll")]
         private static extern bool SetForegroundWindow(IntPtr hWnd);
@@ -35,41 +32,47 @@ namespace CPB.GeradorDeResultados
             _pastaResultadosExibidos = ConfigurationManager.AppSettings["PathResultadosExibidos"].ToString();
             _pastaPaginas = ConfigurationManager.AppSettings["PathPaginas"].ToString();
 
-            if (!Directory.Exists(_pastaResultadosExibidos))
-                Directory.CreateDirectory(_pastaResultadosExibidos);
-
             if (!Directory.Exists(_pastaPaginas))
                 Directory.CreateDirectory(_pastaPaginas);
 
             foreach (var item in Directory.GetFiles(_pasta))
                 File.Delete(item);
 
-            _timer = new System.Timers.Timer();
+            _timerPrincipal = new System.Timers.Timer();
+            _timerPrincipal.Enabled = true;
+            _timerPrincipal.Interval = 100;
+            _timerPrincipal.Elapsed += new ElapsedEventHandler(timerEver_Tick);
 
-            _timer.Enabled = true;
-            _timer.Interval = 3000;
-            _timer.Elapsed += new ElapsedEventHandler(timer_Tick);
+            _timerResultados = new System.Timers.Timer();
+            _timerResultados.Interval = 1000;
+            _timerResultados.Elapsed += new ElapsedEventHandler(timer_Tick);
         }
 
         private void timer_Tick(object sender, EventArgs e)
         {
             if (_horaInicio != default(DateTime) && (DateTime.Now - _horaInicio).Minutes > 0)
             {
-                _timer.Stop();
+                _timerResultados.Stop();
                 CriarPaginaStandBy();
-
                 AtualizarPagina();
             }
 
         }
 
+        private void timerEver_Tick(object sender, EventArgs e)
+        {
+            string[] files = Directory.GetFiles(_pasta);
+            if (files.Length > 0)
+            {
+                _timerPrincipal.Stop();
+                Processar(files[0]);
+            }
+
+            _timerPrincipal.Start();
+        }
+
         public void Iniciar()
         {
-            _watcher = new FileSystemWatcher(_pasta);
-            _watcher.Created += new FileSystemEventHandler(_watcher_Created);
-            _watcher.EnableRaisingEvents = true;
-            _watcher.IncludeSubdirectories = true;
-
             CriarPaginaStandBy();
             AbrirPaginaInicial();
         }
@@ -92,10 +95,22 @@ namespace CPB.GeradorDeResultados
             _internetExplorer.Start();
         }
 
-        private void Processar()
+        private void Processar(string fullPath)
         {
+            FileInfo fileInfo = new FileInfo(fullPath);
+            _linhasArquivo = File.ReadAllLines(fileInfo.FullName, Encoding.UTF8);
+
+            string arquivoParaCopiar = string.Concat(_pastaResultadosExibidos, fileInfo.Name);
+            if (File.Exists(arquivoParaCopiar))
+                File.Delete(arquivoParaCopiar);
+
+            fileInfo.MoveTo(arquivoParaCopiar);
+
+
             _horaInicio = DateTime.Now;
+            _timerResultados.Start();
             AtualizarPaginaDeResultados();
+
         }
 
 
@@ -103,7 +118,7 @@ namespace CPB.GeradorDeResultados
         {
             var resultadoDaProva = new ResultadoDaProva();
 
-            for (int i = 0; i < _linhasArquivo .Length; i++)
+            for (int i = 0; i < _linhasArquivo.Length; i++)
             {
                 if (i == 0)
                     resultadoDaProva.ResolverDadosDaProva(_linhasArquivo[i].Split(','));
@@ -114,32 +129,6 @@ namespace CPB.GeradorDeResultados
             return resultadoDaProva;
         }
 
-        private void _watcher_Created(object sender, FileSystemEventArgs e)
-        {
-            //Stopwatch sw = new Stopwatch();
-            //sw.Start();
-           
-            List<String> lst = new List<string>();
-            using (FileStream fs = new FileStream(e.FullPath, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite))
-            {
-                using (StreamReader sr = new StreamReader(fs))
-                {
-                    while (!sr.EndOfStream)
-                        lst.Add(sr.ReadLine());
-                }
-                fs.Flush();
-            }                
-          
-            //_linhasArquivo = File.ReadAllLines(e.FullPath, Encoding.UTF8);
-            string arquivoParaCopiar = string.Concat(_pastaResultadosExibidos, e.Name);
-            File.Move(e.FullPath, arquivoParaCopiar);
-            Processar();
-            _timer.Start();
-
-
-
-            //File.Delete(e.FullPath);
-        }
 
         private void CriarPaginaStandBy()
         {
